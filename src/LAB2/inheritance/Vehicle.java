094db126d03bc7a8d5e8ce6ea0f962087e439afc
pramishbhandari <pramishbhandari100@gmail.com>
2025-12-14 15:33:15 +0545

    initial commit

---
+++ src/LAB2/inheritance/Vehicle.java
@@ -0,0 +1,13 @@
+package LAB2.inheritance;
+
+public class Vehicle {
+
+    public void start(){
+        System.out.println("Starting..");
+    }
+    public void stop(){
+        System.out.println("Stopping...");
+    }
+
+    
+}
