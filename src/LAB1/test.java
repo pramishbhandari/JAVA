2d5bd4414d139a0e6cca7905b9a0f68eeeaad8cf
pramishbhandari <pramishbhandari100@gmail.com>
2025-12-14 15:15:05 +0545

    initial commit

---
+++ src/LAB1/test.java
@@ -0,0 +1,11 @@
+public class test{
+public static void main(String[] args){
+for(int i=1;i<11;i++){
+System.out.println("Multiplication table of "+i);
+for(int j=1;j<11;j++){
+System.out.println(i+"x"+j+"="+i*j);
+}
+}
+
+}
+}
