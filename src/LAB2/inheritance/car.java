f3001f9f88578f4bfc763eaf5a056a75a5b2129a
pramishbhandari <pramishbhandari100@gmail.com>
2025-12-18 14:06:15 +0545

    first commit

---
+++ src/LAB2/inheritance/car.java
@@ -0,0 +1,12 @@
+
+package LAB2.inheritance;
+
+public class car extends Vehicle {
+    public static void main(String[] args) {
+
+        car car = new car();
+        car.start();
+
+    }
+
+}
---
+++ src/LAB2/inheritance/emp.java
@@ -0,0 +1,12 @@
+package LAB2.inheritance;
+public class emp{
+    int rate=800;
+
+
+    int calculatesal(int hr) {
+        return hr * rate;
+    }
+
+
+}
+
